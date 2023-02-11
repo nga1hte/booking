3f5e595e6aaddc9a27ce36335b1b749dbac7392d
nga1hte <ngaihte@outlook.com>
2023-02-11 22:08:40 +0530

    fixed testing for store tx

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -30,15 +30,19 @@ func TestBookingTx(t *testing.T) {
 		BookingEnds:   booking.BookEnds,
 	}
 
-	result, err := store.BookingTx(context.Background(), arg)
+	result1, err := store.BookingTx(context.Background(), arg)
 	require.NoError(t, err)
 
-	userRes := result.User
+	userRes := result1.User
 	require.Equal(t, userRes.ID, user.ID)
 
-	bookingRes := result.Booking
+	bookingRes := result1.Booking
 	require.Equal(t, bookingRes.BookedBy, user.ID)
 	require.WithinDuration(t, bookingRes.BookStarts, arg.BookingStarts, 1*time.Second)
 	require.WithinDuration(t, bookingRes.BookEnds, arg.BookingEnds, 1*time.Second)
+
+	_, err2 := store.BookingTx(context.Background(), arg)
+	require.Error(t, err2)
+	require.EqualError(t, err2, "slot already booked")
 
 }
