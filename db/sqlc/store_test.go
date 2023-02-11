7041b32fad5082fe859a77bfba5a875e1600df01
nga1hte <ngaihte@outlook.com>
2023-02-11 20:21:52 +0530

    changes to store tx

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -41,8 +41,4 @@ func TestBookingTx(t *testing.T) {
 	require.WithinDuration(t, bookingRes.BookStarts, arg.BookingStarts, 1*time.Second)
 	require.WithinDuration(t, bookingRes.BookEnds, arg.BookingEnds, 1*time.Second)
 
-	result, err = store.BookingTx(context.Background(), arg)
-	require.Error(t, err)
-	require.ErrorContains(t, err, "slot already booked")
-
 }
